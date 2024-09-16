645ad6ce917e29e47397356e061be6aa3e6bb150
Hemanth <atluri.hk4446@gmail.com>
2024-09-16 20:08:24 +0530

    Start

---
+++ JavaScript/A.js
@@ -0,0 +1,7 @@
+var My_Name = "Hemanth";
+My_Name = "Hemanth Kumar Atluri";
+alert(My_Name);
+
+
+var Your_Name = prompt("What is your name?");
+alert("my name is " + My_Name + ", Thanks for welcoming me, " + Your_Name + "!");
